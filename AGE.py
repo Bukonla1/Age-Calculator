e81537087588fc40b1e018a6e4439bd1efc69376
Bukonla <bukonla.osokoya@gmail.com>
2022-02-09 13:48:03 +0100

    my first commit

--- AGE.py
+++ AGE.py
@@ -82,4 +82,10 @@ clear.grid(row=4,column=2)
 root.mainloop()
 
 
-
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)  
